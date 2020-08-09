64290ae3a86cff76716d07cab9784c9684294a0f
suzukalight <mail@mkubara.com>
2020-08-09 23:38:22 +0900

    Add Todo schema

--- src/domain-model/src/types.ts
+++ src/domain-model/src/types.ts
@@ -1,5 +1,5 @@
 export type Maybe<T> = T | null;
-export type Exact<T extends { [key: string]: any }> = { [K in keyof T]: T[K] };
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -7,18 +7,37 @@ export type Scalars = {
   Boolean: boolean;
   Int: number;
   Float: number;
-  Date: any;
+  DateTime: Date;
 };
 
 
 export type Query = {
   __typename?: 'Query';
   user?: Maybe<User>;
+  todo?: Maybe<Todo>;
 };
 
 
 export type QueryUserArgs = {
   id: Scalars['ID'];
+};
+
+
+export type QueryTodoArgs = {
+  id: Scalars['ID'];
+};
+
+export enum TodoStatus {
+  Undone = 'UNDONE',
+  Done = 'DONE'
+}
+
+export type Todo = {
+  __typename?: 'Todo';
+  id: Scalars['ID'];
+  title: Scalars['String'];
+  status: TodoStatus;
+  dueDate?: Maybe<Scalars['DateTime']>;
 };
 
 export type User = {
