5d8312e9397b1543cf812a2a48e570e93e380a6b
suzukalight <mail@mkubara.com>
2020-08-09 22:07:40 +0900

    Update user entity to use value object

--- src/domain-model/src/entity/user/validator.ts
+++
@@ -1,5 +0,0 @@
-import { User, Scalars } from '../../types';
-
-export const isValid = (user: User) => {
-  return !!user.id;
-};
---
+++ src/domain-model/src/user/UserEntity.ts
@@ -0,0 +1,51 @@
+import { User } from '../types';
+import { ID } from '../common/ID';
+import { Email } from '../common/Email';
+import { PropertyRequiredError } from 'common/error/PropertyRequired';
+
+export const isValidArguments = (user: User) => {
+  if (!user) throw new PropertyRequiredError('user');
+  if (!user.id) throw new PropertyRequiredError('id');
+  if (!user.email) throw new PropertyRequiredError('email');
+  return true;
+};
+
+export class UserEntity {
+  private id: ID;
+  private email: Email;
+
+  constructor(user: User) {
+    isValidArguments(user);
+    this.id = new ID(user.id);
+    this.email = new Email(user.email!);
+  }
+
+  getID() {
+    return this.id;
+  }
+
+  getEmail() {
+    return this.email;
+  }
+
+  setId(id: ID) {
+    this.id = id;
+    this.isValid();
+  }
+
+  setEmail(email: Email) {
+    this.email = email;
+    this.isValid();
+  }
+
+  toJSON(): User {
+    return {
+      id: this.id.toString(),
+      email: this.email.toString(),
+    };
+  }
+
+  isValid(): boolean {
+    return isValidArguments(this.toJSON());
+  }
+}
