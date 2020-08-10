68132d1a86f82919fec1afe96515892534ee754e
suzukalight <mail@mkubara.com>
2020-08-11 00:57:37 +0900

    Add apollo-server to infrastructure

---
+++ src/backend/src/infrastructure/apollo-server/index.ts
@@ -0,0 +1,50 @@
+import express from 'express';
+import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
+import { loadSchemaSync } from '@graphql-tools/load';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { addResolversToSchema } from '@graphql-tools/schema';
+import dotenv from 'dotenv';
+import http from 'http';
+import path from 'path';
+
+import { resolvers } from './resolvers';
+
+dotenv.config();
+
+//
+// create graphql schema
+//
+const schema = loadSchemaSync(path.join(__dirname, '../../../../schema/schema.graphql'), {
+  loaders: [new GraphQLFileLoader()],
+});
+const schemaWithResolvers = addResolversToSchema({
+  schema,
+  resolvers,
+});
+
+//
+// create express server
+//
+const app = express();
+app.use(cors());
+
+const server = new ApolloServer({
+  schema: schemaWithResolvers,
+});
+
+//
+// apply graphql server to express
+//
+server.applyMiddleware({ app, path: '/graphql' });
+
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+//
+// run server
+//
+const port = 3000;
+httpServer.listen({ port }, () => {
+  console.log(`Apollo Server on http://localhost:${port}/graphql`);
+});
