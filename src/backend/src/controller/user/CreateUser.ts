09d2c93fd557e6a0a34f26bc3588bfb8294aa0e7
suzukalight <mail@mkubara.com>
2020-08-11 00:55:14 +0900

    Add controller of CreateUser

---
+++ src/backend/src/controller/user/CreateUser.ts
@@ -0,0 +1,15 @@
+import { CreateUserRequest } from 'schema/types';
+
+import { CreateUserUseCase } from '../../usecase/user/usecase.interface';
+
+export class CreateUserController {
+  private createUserUseCase: CreateUserUseCase;
+
+  constructor(createUserUseCase: CreateUserUseCase) {
+    this.createUserUseCase = createUserUseCase;
+  }
+
+  public async handle(request: CreateUserRequest) {
+    this.createUserUseCase.handle(request);
+  }
+}
