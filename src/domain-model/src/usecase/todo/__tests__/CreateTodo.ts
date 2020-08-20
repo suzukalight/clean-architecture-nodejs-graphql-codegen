e148cc8e1edbf2db502a2a23f57c9e879d21ec32
suzukalight <mail@mkubara.com>
2020-08-20 22:12:13 +0900

    Add test for CreateTodo usecase

---
+++ src/domain-model/src/usecase/todo/__tests__/CreateTodo.ts
@@ -0,0 +1,49 @@
+import { NotFoundError } from 'common/error/NotFound';
+
+import { CreateTodoInteractor } from '../CreateTodo';
+import { MockTodoRepository } from '../__mocks__/MockTodoRepository';
+import { MockCreateTodoPresenter } from '../__mocks__/MockTodoPresenter';
+import { MockUserRepository } from '../../user/__mocks__/MockUserRepository';
+
+/**
+ * オーナーを1名作成しておく
+ */
+const setup = async () => {
+  // user repository
+  const userRepository = new MockUserRepository();
+  const userEntity = await userRepository.create({ email: 'target@email.com' });
+  const ownerId = userEntity.getId().toString();
+
+  // todo interactor
+  const todoRepository = new MockTodoRepository();
+  const presenter = new MockCreateTodoPresenter();
+  const interactor = new CreateTodoInteractor(todoRepository, userRepository, presenter);
+
+  return { ownerId, interactor, presenter };
+};
+
+describe('CreateTodoInteractor', () => {
+  it('リクエストを処理し、新しいエンティティを生成できた', async () => {
+    const { ownerId, interactor, presenter } = await setup();
+    const request = { ownerId, title: 'todo #1' };
+
+    await interactor.handle(request);
+
+    // response として request で指定したデータが得られた
+    const response = presenter.getResponse();
+    expect(response?.todo?.ownerId).toBe(request.ownerId);
+    expect(response?.todo?.title).toBe(request.title);
+  });
+
+  it('存在しないオーナーを指定したため、失敗した', async () => {
+    const { interactor } = await setup();
+    const request = { ownerId: '99999', title: 'todo #1' };
+
+    try {
+      await interactor.handle(request);
+      expect(true).toBeFalsy();
+    } catch (e) {
+      expect(e).toBeInstanceOf(NotFoundError);
+    }
+  });
+});
