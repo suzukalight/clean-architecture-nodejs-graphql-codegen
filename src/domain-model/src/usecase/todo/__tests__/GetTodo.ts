4af91cb09c8b1fe48fd27715e852a312b3aaa057
suzukalight <mail@mkubara.com>
2020-08-20 22:12:05 +0900

    Add test for GetTodo usecase

---
+++ src/domain-model/src/usecase/todo/__tests__/GetTodo.ts
@@ -0,0 +1,47 @@
+import { GetTodoInteractor } from '../GetTodo';
+import { MockTodoRepository } from '../__mocks__/MockTodoRepository';
+import { MockGetTodoPresenter } from '../__mocks__/MockTodoPresenter';
+import { MockUserRepository } from '../../user/__mocks__/MockUserRepository';
+
+/**
+ * TODOを1つ作成しておく
+ */
+const setup = async () => {
+  // user repository
+  const userRepository = new MockUserRepository();
+  const userEntity = await userRepository.create({ email: 'target@email.com' });
+  const ownerId = userEntity.getId().toString();
+
+  // create todo
+  const repository = new MockTodoRepository();
+  const todoEntity = await repository.create({ ownerId, title: 'todo #1' });
+  const todoId = todoEntity.getId().toString();
+
+  // interactor
+  const presenter = new MockGetTodoPresenter();
+  const interactor = new GetTodoInteractor(repository, presenter);
+
+  return { todoId, interactor, presenter };
+};
+
+describe('GetTodoInteractor', () => {
+  it('リクエストを処理し、ユーザを取得できた', async () => {
+    const { todoId, interactor, presenter } = await setup();
+
+    await interactor.handle(todoId);
+
+    // response として request で指定したデータが得られた
+    const response = presenter.getResponse();
+    expect(response?.id).toBe(todoId);
+  });
+
+  it('存在しないIDを指定したため、nullが返された', async () => {
+    const { interactor, presenter } = await setup();
+
+    await interactor.handle('99999');
+
+    // response として null が得られた
+    const response = presenter.getResponse();
+    expect(response).toBeNull();
+  });
+});
