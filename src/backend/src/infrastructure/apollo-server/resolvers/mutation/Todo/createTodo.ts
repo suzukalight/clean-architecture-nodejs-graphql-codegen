d760d759d138b234248b900ff80ef36a8b1c0f71
suzukalight <mail@mkubara.com>
2020-08-23 10:34:50 +0900

    Apply policy to createTodo
    Add: Memberロールを持っていない場合はdenyする

--- src/backend/src/infrastructure/apollo-server/resolvers/mutation/Todo/createTodo.ts
+++ src/backend/src/infrastructure/apollo-server/resolvers/mutation/Todo/createTodo.ts
@@ -1,13 +1,16 @@
 import { MutationResolvers } from 'schema/types';
 import { CreateTodoInteractor } from 'domain-model/src/usecase/todo/CreateTodo';
 
-import { ApolloServerContext } from '../../type';
+import { ApolloServerContext } from '../../../type';
 import { TodoRepository } from '../../../../../repository/typeorm/Todo';
 import { CreateTodoPresenter } from '../../../../../presenter/todo/CreateTodo';
 import { UserRepository } from '../../../../../repository/typeorm/User';
+import { allowWhenActorHasMemberRole } from '../../../authority/policy';
 
 export const createTodo: MutationResolvers<ApolloServerContext> = {
-  createTodo: async (_parent, args, { dbConnection }) => {
+  createTodo: async (_parent, args, { dbConnection, actor }) => {
+    allowWhenActorHasMemberRole(actor);
+
     const userRepository = new UserRepository(dbConnection);
     const todoRepository = new TodoRepository(dbConnection);
     const presenter = new CreateTodoPresenter();
