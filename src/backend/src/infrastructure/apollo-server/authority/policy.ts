4194c4b4378187490f58ba571268df04c319d5ac
suzukalight <mail@mkubara.com>
2020-08-23 18:59:44 +0900

    Fix

--- src/backend/src/infrastructure/apollo-server/authority/policy.ts
+++ src/backend/src/infrastructure/apollo-server/authority/policy.ts
@@ -3,12 +3,12 @@ import { AuthenticationFailedError } from 'common/error/AuthenticationFailed';
 import { Maybe, Role } from 'schema/types';
 import { UserEntity } from 'domain-model/src/entity/user/UserEntity';
 
-export const denyUnauthorized = (actor?: Maybe<UserEntity>) => {
+export const denyUnauthenticated = (actor: Maybe<UserEntity>) => {
   if (!actor) throw new AuthenticationFailedError();
 };
 
-export const denyWhenActorHasOnlyAnonymousRole = (actor?: Maybe<UserEntity>) => {
-  denyUnauthorized(actor);
+export const denyWhenActorHasOnlyAnonymousRole = (actor: Maybe<UserEntity>) => {
+  denyUnauthenticated(actor);
 
   // anonumous ではないロールを1つも持っていない
   if (!actor?.getRoles().some((role) => !role.isEqual(Role.Anonymous))) {
@@ -16,8 +16,8 @@ export const denyWhenActorHasOnlyAnonymousRole = (actor?: Maybe<UserEntity>) => 
   }
 };
 
-export const allowWhenActorHasMemberRole = (actor?: Maybe<UserEntity>) => {
-  denyUnauthorized(actor);
+export const allowWhenActorHasMemberRole = (actor: Maybe<UserEntity>) => {
+  denyUnauthenticated(actor);
 
   if (!actor?.getRoles().some((role) => role.isEqual(Role.Member))) {
     throw new UnauthorizedError();
