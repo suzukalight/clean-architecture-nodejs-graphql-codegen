e3f052c7127c973b7efb36f3565f4a6b02fb2b6c
suzukalight <mail@mkubara.com>
2020-08-30 21:22:20 +0900

    Add fluentui test page

--- src/frontend/src/pages/fluentui.tsx
+++ src/frontend/src/pages/fluentui.tsx
@@ -1,13 +1,49 @@
 import * as React from 'react';
-import { Checkbox, PrimaryButton, Slider, TextField, Toggle } from '@fluentui/react';
+import {
+  Checkbox,
+  IRenderFunction,
+  ICheckboxProps,
+  PrimaryButton,
+  Slider,
+  TextField,
+  Toggle,
+} from '@fluentui/react';
+
+import LeftNav from '../components/LeftNav';
+
+import styles from './fluentui.module.scss';
+import AddTodo from '../components/AddTodo';
+import TodoList from '../components/TodoList';
+
+const LeftPane = () => (
+  <div className={styles.left}>
+    <LeftNav />
+  </div>
+);
+
+const CheckBoxLabelRenderer: IRenderFunction<{}> = (props: ICheckboxProps) => (
+  <div>'Helllllloooooo'</div>
+);
+
+const MainPane = () => (
+  <div className={styles.main}>
+    <div className={styles.wrap}>
+      <TodoList todos={['todo #1', 'todo #2']} />
+      <AddTodo onSubmit={(title) => alert(title)} />
+    </div>
+  </div>
+);
 
 const Index = () => (
-  <div>
-    <PrimaryButton>Hello, world</PrimaryButton>
-    <Toggle defaultChecked label="Hello" />
-    <TextField defaultValue="hello" />
-    <Checkbox defaultChecked label="Hello" />
-    <Slider defaultValue={50} max={100} />
+  <div className="ms-Grid" dir="ltr">
+    <div className="ms-Grid-row">
+      <div className="ms-Grid-col ms-sm3">
+        <LeftPane />
+      </div>
+      <div className="ms-Grid-col ms-sm9">
+        <MainPane />
+      </div>
+    </div>
   </div>
 );
 
