1ff92f72361c68b83b6970affaffc37811a08bf2
suzukalight <mail@mkubara.com>
2020-09-06 18:02:19 +0900

    Add validation to login page

--- src/frontend/src/pages/login/index.tsx
+++ src/frontend/src/pages/login/index.tsx
@@ -1,10 +1,13 @@
 import React, { useCallback } from 'react';
-import { Stack, TextField, PrimaryButton, Text, IStackTokens } from '@fluentui/react';
+import { Stack, PrimaryButton, Text, IStackTokens } from '@fluentui/react';
+import { Formik, Form, Field } from 'formik';
 
 import {
   useSignInEmailPasswordMutation,
   SignInEmailPasswordRequest,
 } from '../../generated/graphql-client';
+import { FormikTextField } from '../../components/_formik/TextField';
+import { loginValidationSchema } from './validation';
 
 import styles from './index.module.scss';
 
@@ -15,38 +18,57 @@ const spacingToken: IStackTokens = {
 
 type SubmitFunction = (input: SignInEmailPasswordRequest) => void;
 type LoginFormProps = {
+  initialValues: SignInEmailPasswordRequest;
   onSubmit: SubmitFunction;
 };
 
-const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => (
-  <Stack>
-    <Stack tokens={spacingToken}>
-      <TextField label="email" placeholder="email" />
-    </Stack>
-    <Stack tokens={spacingToken}>
-      <TextField type="password" label="password" placeholder="password" />
-    </Stack>
-    <Stack tokens={spacingToken}>
-      <PrimaryButton
-        onClick={() => onSubmit({ email: 'admin@email.com', password: 'password1234' })}
-      >
-        ログイン
-      </PrimaryButton>
-    </Stack>
-  </Stack>
+const LoginForm: React.FC<LoginFormProps> = ({ initialValues, onSubmit }) => (
+  <Formik
+    onSubmit={onSubmit}
+    initialValues={initialValues}
+    validationSchema={loginValidationSchema}
+  >
+    {({ isSubmitting, isValid }) => (
+      <Form>
+        <Stack>
+          <Stack tokens={spacingToken}>
+            <Field component={FormikTextField} name="email" label="email" placeholder="email" />
+          </Stack>
+          <Stack tokens={spacingToken}>
+            <Field
+              component={FormikTextField}
+              name="password"
+              type="password"
+              label="password"
+              placeholder="password"
+            />
+          </Stack>
+          <Stack tokens={spacingToken}>
+            <PrimaryButton type="submit" disabled={isSubmitting || !isValid}>
+              ログイン
+            </PrimaryButton>
+          </Stack>
+        </Stack>
+      </Form>
+    )}
+  </Formik>
 );
 
 const Login = () => {
-  const [signInEmailPasswordMutation, { data, loading, error }] = useSignInEmailPasswordMutation();
+  const [signInEmailPasswordMutation] = useSignInEmailPasswordMutation();
 
   const handleSubmit = useCallback<SubmitFunction>(
     async (input) => {
-      console.log(input);
       const result = await signInEmailPasswordMutation({ variables: { input } }).catch(() => {});
       console.log(result);
     },
     [signInEmailPasswordMutation],
   );
+
+  const initialValues = {
+    email: '',
+    password: '',
+  };
 
   return (
     <Stack className={styles.wrap}>
@@ -55,7 +77,7 @@ const Login = () => {
           <Text variant="xLarge">TodoApp</Text>
         </Stack>
         <Stack>
-          <LoginForm onSubmit={handleSubmit} />
+          <LoginForm initialValues={initialValues} onSubmit={handleSubmit} />
         </Stack>
       </Stack>
     </Stack>
