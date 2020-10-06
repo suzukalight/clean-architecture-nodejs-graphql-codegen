69e9bb9feebd7b350c4002e47e0615efa80ae1f9
suzukalight <mail@mkubara.com>
2020-10-06 11:03:17 +0900

    Update entity resolvers

--- src/backend/src/infrastructure/apollo-server/resolvers/entity/Todo.ts
+++ src/backend/src/infrastructure/apollo-server/resolvers/entity/Todo.ts
@@ -1,11 +1,12 @@
 import { TodoResolvers } from 'schema';
 
-import { UserRepository } from '../../../../repository/typeorm/user/repository/User';
+import { GqlUserRepository } from '../../../../repository/typeorm/user/repository/User';
+import { toGqlUser } from '../../../../presenter/utils/converter/user';
 
 export const Todo: TodoResolvers = {
   owner: async (parent, args, ctx) => {
-    const repository = new UserRepository(ctx.dbConnection);
+    const repository = new GqlUserRepository(ctx.dbConnection);
     const userEntity = await repository.getById(parent.ownerId);
-    return userEntity ? userEntity.toJSON() : null;
+    return userEntity ? toGqlUser(userEntity.toDto()) : null;
   },
 };
--- src/backend/src/infrastructure/apollo-server/resolvers/entity/User.ts
+++ src/backend/src/infrastructure/apollo-server/resolvers/entity/User.ts
@@ -1,11 +1,11 @@
 import { UserResolvers } from 'schema';
 
-import { TodoRepository } from '../../../../repository/typeorm/todo/repository/Todo';
+import { GqlTodoRepository } from '../../../../repository/typeorm/todo/repository/Todo';
 
 export const User: UserResolvers = {
-  todos: async (parent, args, ctx) => {
-    const repository = new TodoRepository(ctx.dbConnection);
+  todos: async (parent, _args, ctx) => {
+    const repository = new GqlTodoRepository(ctx.dbConnection);
     const todoEntities = await repository.allByOwnerId(parent.id);
-    return todoEntities ? todoEntities.map((todo) => todo.toJSON()) : null;
+    return todoEntities ? todoEntities.map((todo) => todo.toDto()) : null;
   },
 };
