97fbd24c8b77c9b88cd38a723f0ada87ca8a5052
suzukalight <mail@mkubara.com>
2020-10-06 11:01:28 +0900

    Update query resolvers

--- src/backend/src/infrastructure/apollo-server/resolvers/query/Todo/todo.ts
+++ src/backend/src/infrastructure/apollo-server/resolvers/query/Todo/todo.ts
@@ -2,18 +2,18 @@ import { QueryResolvers } from 'schema';
 import { GetTodoInteractor, allowOnlyWhenActorHasMemberRole } from 'domain-model';
 
 import { ApolloServerContext } from '../../../types';
-import { GetTodoPresenter } from '../../../../../presenter/todo/GetTodo';
-import { TodoRepository } from '../../../../../repository/typeorm/todo/repository/Todo';
+import { GqlGetTodoPresenter } from '../../../../../presenter/todo/GetTodo';
+import { GqlTodoRepository } from '../../../../../repository/typeorm/todo/repository/Todo';
 
 export const todo: QueryResolvers<ApolloServerContext> = {
   todo: async (_parent, args, { dbConnection, actor }) => {
     allowOnlyWhenActorHasMemberRole(actor);
 
-    const repository = new TodoRepository(dbConnection);
-    const presenter = new GetTodoPresenter();
+    const repository = new GqlTodoRepository(dbConnection);
+    const presenter = new GqlGetTodoPresenter();
     const usecase = new GetTodoInteractor(repository, presenter);
 
-    await usecase.handle(args.id);
+    await usecase.handle({ id: args.id });
 
     return presenter.getResponse();
   },
--- src/backend/src/infrastructure/apollo-server/resolvers/query/User/user.ts
+++ src/backend/src/infrastructure/apollo-server/resolvers/query/User/user.ts
@@ -2,18 +2,18 @@ import { QueryResolvers } from 'schema';
 import { GetUserInteractor, allowOnlyWhenActorHasMemberRole } from 'domain-model';
 
 import { ApolloServerContext } from '../../../types';
-import { GetUserPresenter } from '../../../../../presenter/user/GetUser';
-import { UserRepository } from '../../../../../repository/typeorm/user/repository/User';
+import { GqlGetUserPresenter } from '../../../../../presenter/user/GetUser';
+import { GqlUserRepository } from '../../../../../repository/typeorm/user/repository/User';
 
 export const user: QueryResolvers<ApolloServerContext> = {
   user: async (_parent, args, { dbConnection, actor }) => {
     allowOnlyWhenActorHasMemberRole(actor);
 
-    const repository = new UserRepository(dbConnection);
-    const presenter = new GetUserPresenter();
+    const repository = new GqlUserRepository(dbConnection);
+    const presenter = new GqlGetUserPresenter();
     const usecase = new GetUserInteractor(repository, presenter);
 
-    await usecase.handle(args.id);
+    await usecase.handle({ id: args.id });
 
     return presenter.getResponse();
   },
