50d50cf8046b8b3348f73d3b4cfa5dd8159c3533
suzukalight <mail@mkubara.com>
2020-10-12 21:01:53 +0900

    Add mutation resolver of deleteUser

---
+++ src/backend/src/infrastructure/apollo-server/resolvers/mutation/User/deleteUser.ts
@@ -0,0 +1,20 @@
+import { MutationResolvers } from 'schema';
+import { DeleteUserInteractor, allowOnlyWhenActorHasMemberRole } from 'domain-model';
+
+import { ApolloServerContext } from '../../../types';
+import { GqlUserRepository } from '../../../../../repository/typeorm/user/repository/User';
+import { GqlDeleteUserPresenter } from '../../../../../presenter/user/DeleteUser';
+
+export const deleteUser: MutationResolvers<ApolloServerContext> = {
+  deleteUser: async (_parent, args, { dbConnection, actor }) => {
+    allowOnlyWhenActorHasMemberRole(actor);
+
+    const repository = new GqlUserRepository(dbConnection);
+    const presenter = new GqlDeleteUserPresenter();
+    const usecase = new DeleteUserInteractor(repository, presenter);
+
+    await usecase.handle(args.input!, actor!);
+
+    return presenter.getResponse();
+  },
+};
