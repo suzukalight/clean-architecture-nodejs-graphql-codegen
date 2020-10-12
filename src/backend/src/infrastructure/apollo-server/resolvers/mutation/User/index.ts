94bb0a0e2250c44d54acad2fe26b9037aad5485b
suzukalight <mail@mkubara.com>
2020-10-13 00:06:59 +0900

    Export deleteUser resolver

--- src/backend/src/infrastructure/apollo-server/resolvers/mutation/User/index.ts
+++ src/backend/src/infrastructure/apollo-server/resolvers/mutation/User/index.ts
@@ -2,9 +2,11 @@ import { MutationResolvers } from 'schema';
 
 import { ApolloServerContext } from '../../../types';
 import { createUser } from './createUser';
+import { deleteUser } from './deleteUser';
 import { updateUserRoles } from './updateUserRoles';
 
 export const User: MutationResolvers<ApolloServerContext> = {
   ...createUser,
   ...updateUserRoles,
+  ...deleteUser,
 };
