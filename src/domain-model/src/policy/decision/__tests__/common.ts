182cfdac059bdf5f252827bfccc649dc5b2e3f56
suzukalight <mail@mkubara.com>
2020-10-12 17:20:57 +0900

    Add test for allowOnlyWhenActorHasMemberRole

--- src/domain-model/src/policy/decision/__tests__/common.ts
+++ src/domain-model/src/policy/decision/__tests__/common.ts
@@ -2,20 +2,24 @@ import { AuthenticationFailedError } from 'common';
 
 import { RoleTypes } from '../../../entity/common/Role';
 import { UserEntity } from '../../../entity/user/UserEntity';
-import { denyUnauthenticated, denyWhenActorHasOnlyAnonymousRole } from '../common';
+import {
+  denyUnauthenticated,
+  denyWhenActorHasOnlyAnonymousRole,
+  allowOnlyWhenActorHasMemberRole,
+} from '../common';
 
 describe('PolicyDecisionCommon', () => {
   describe('denyUnauthenticated', () => {
-    test('actorが設定されていればOK', async () => {
+    test('成功：actorが設定されている', async () => {
       const actor = new UserEntity({ id: '1', email: 'aaa@bb.com', roles: [RoleTypes.Member] });
       denyUnauthenticated(actor);
     });
 
-    test('actorが設定されていないため、失敗した', async () => {
+    test('失敗：actorが設定されていない', async () => {
       expect(() => denyUnauthenticated(null)).toThrow(AuthenticationFailedError);
     });
 
-    test('actorが設定されていないため、失敗した。（メッセージつき）', async () => {
+    test('失敗：actorが設定されていない（メッセージつき）', async () => {
       expect(() => denyUnauthenticated(null, 'ログインしてください')).toThrow(
         /ログインしてください/,
       );
@@ -23,12 +27,12 @@ describe('PolicyDecisionCommon', () => {
   });
 
   describe('denyWhenActorHasOnlyAnonymousRole', () => {
-    test('actorが設定されていればOK', async () => {
+    test('成功：Memberロールが設定されている', async () => {
       const actor = new UserEntity({ id: '1', email: 'aaa@bb.com', roles: [RoleTypes.Member] });
       denyWhenActorHasOnlyAnonymousRole(actor);
     });
 
-    test('Anonymous+Memberは成功', async () => {
+    test('成功：Anonymous+Member', async () => {
       const actor = new UserEntity({
         id: '1',
         email: 'aaa@bb.com',
@@ -37,17 +41,62 @@ describe('PolicyDecisionCommon', () => {
       expect(() => denyWhenActorHasOnlyAnonymousRole(null)).toThrow(AuthenticationFailedError);
     });
 
-    test('Anonymousロールしか持っていないため、失敗した', async () => {
+    test('失敗：Anonymousロールしか持っていない', async () => {
       const actor = new UserEntity({ id: '1', email: 'aaa@bb.com', roles: [RoleTypes.Anonymous] });
       expect(() => denyWhenActorHasOnlyAnonymousRole(null)).toThrow(AuthenticationFailedError);
     });
 
-    test('actorが設定されていないため、失敗した', async () => {
+    test('失敗：actorが設定されていない', async () => {
       expect(() => denyWhenActorHasOnlyAnonymousRole(null)).toThrow(AuthenticationFailedError);
     });
 
-    test('actorが設定されていないため、失敗した。（メッセージつき）', async () => {
+    test('失敗：actorが設定されていない（メッセージつき）', async () => {
       expect(() => denyWhenActorHasOnlyAnonymousRole(null, 'メンバー権限をつけてください')).toThrow(
+        /メンバー権限をつけてください/,
+      );
+    });
+  });
+
+  describe('allowOnlyWhenActorHasMemberRole', () => {
+    test('成功：Memberロールが設定されている', async () => {
+      const actor = new UserEntity({ id: '1', email: 'aaa@bb.com', roles: [RoleTypes.Member] });
+      allowOnlyWhenActorHasMemberRole(actor);
+    });
+
+    test('成功：Anonymous+Member', async () => {
+      const actor = new UserEntity({
+        id: '1',
+        email: 'aaa@bb.com',
+        roles: [RoleTypes.Anonymous, RoleTypes.Member],
+      });
+      expect(() => allowOnlyWhenActorHasMemberRole(null)).toThrow(AuthenticationFailedError);
+    });
+
+    test('失敗：Anonymousロールしか持っていない', async () => {
+      const actor = new UserEntity({ id: '1', email: 'aaa@bb.com', roles: [RoleTypes.Anonymous] });
+      expect(() => allowOnlyWhenActorHasMemberRole(null)).toThrow(AuthenticationFailedError);
+    });
+
+    test('失敗：Adminロールしか持っていない', async () => {
+      const actor = new UserEntity({ id: '1', email: 'aaa@bb.com', roles: [RoleTypes.Admin] });
+      expect(() => allowOnlyWhenActorHasMemberRole(null)).toThrow(AuthenticationFailedError);
+    });
+
+    test('失敗：Anonymous+Admin', async () => {
+      const actor = new UserEntity({
+        id: '1',
+        email: 'aaa@bb.com',
+        roles: [RoleTypes.Anonymous, RoleTypes.Admin],
+      });
+      expect(() => allowOnlyWhenActorHasMemberRole(null)).toThrow(AuthenticationFailedError);
+    });
+
+    test('失敗：actorが設定されていない', async () => {
+      expect(() => allowOnlyWhenActorHasMemberRole(null)).toThrow(AuthenticationFailedError);
+    });
+
+    test('失敗：actorが設定されていない（メッセージつき）', async () => {
+      expect(() => allowOnlyWhenActorHasMemberRole(null, 'メンバー権限をつけてください')).toThrow(
         /メンバー権限をつけてください/,
       );
     });
