36aba35076aace7e2c21f4a43206678b798a2714
suzukalight <mail@mkubara.com>
2020-10-18 09:57:47 +0900

    Add tests for email entity

---
+++ src/domain-model/src/entity/common/Email/__tests__/index.ts
@@ -0,0 +1,55 @@
+import { IllegalArgumentError, PropertyRequiredError } from 'common';
+import { Email, denyIllegalEmail } from '../';
+
+describe('EmailEntity', () => {
+  describe('denyIllegalEmail', () => {
+    test('OK: Emailエンティティを生成できた', () => {
+      expect(denyIllegalEmail('aaa@email.com')).toBeDefined();
+    });
+    test('NG: emailが空文字', () => {
+      expect(() => denyIllegalEmail('')).toThrow(PropertyRequiredError);
+    });
+    test('NG: emailが不正', () => {
+      expect(() => denyIllegalEmail('invalidemailaddress')).toThrow(IllegalArgumentError);
+    });
+  });
+
+  describe('constructor', () => {
+    test('OK: Emailエンティティを生成できた', () => {
+      expect(new Email('aaa@email.com')).toBeDefined();
+    });
+    test('NG: emailが空文字', () => {
+      expect(() => new Email('')).toThrow(PropertyRequiredError);
+    });
+    test('NG: emailが不正', () => {
+      expect(() => new Email('invalidemailaddress')).toThrow(IllegalArgumentError);
+    });
+  });
+
+  describe('getters', () => {
+    const emailString = 'aaa@email.com';
+    const emailEntity = new Email(emailString);
+
+    test('OK: getEmail', () => {
+      expect(emailEntity.getEmail()).toBe(emailString);
+    });
+    test('OK: toString', () => {
+      expect(emailEntity.toString()).toBe(emailString);
+    });
+    test('OK: isEqual(string)', () => {
+      expect(emailEntity.isEqual(emailString)).toBeTruthy();
+    });
+    test('OK: isEqual(Email)', () => {
+      expect(emailEntity.isEqual(new Email(emailString))).toBeTruthy();
+    });
+    test('NG: !== isEqual(string)', () => {
+      expect(emailEntity.isEqual('another-address@email.com')).toBeFalsy();
+    });
+    test('NG: !== isEqual(Email)', () => {
+      expect(emailEntity.isEqual(new Email('another-address@email.com'))).toBeFalsy();
+    });
+    test('NG: !== isEqual(unknown)', () => {
+      expect(() => emailEntity.isEqual({} as any)).toThrow(IllegalArgumentError);
+    });
+  });
+});
