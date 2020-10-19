2a4df7df630314134d0fa9b7035b35e908df2720
suzukalight <mail@mkubara.com>
2020-10-20 00:37:52 +0900

    Add tests for deleteUser mutation

---
+++ src/backend/tests/integration/User/__tests__/deleteUser.ts
@@ -0,0 +1,126 @@
+import { ApolloServerTestClient } from 'apollo-server-testing';
+import { gql } from 'apollo-server-express';
+
+import { SqliteDbConnection } from '../../setup/database';
+import { createApolloServerForTesting } from '../../setup/apollo-server';
+import { RoleTypes, UserEntity } from 'domain-model';
+
+const DELETE_USER = gql`
+  # Write your query or mutation here
+  mutation DeleteUser($input: DeleteUserRequest) {
+    deleteUser(input: $input) {
+      user {
+        id
+        email
+      }
+    }
+  }
+`;
+
+describe('deleteUser', () => {
+  describe('Admin', () => {
+    const actor = { id: '1', email: 'admin@email.com', roles: [RoleTypes.Admin] };
+    const sqliteDbConnection = new SqliteDbConnection();
+    let testClient: ApolloServerTestClient | undefined;
+
+    beforeAll(async () => {
+      const connection = await sqliteDbConnection.connect();
+      testClient = await createApolloServerForTesting(connection, actor);
+      await sqliteDbConnection.seedAll();
+    });
+
+    afterAll(async () => {
+      await sqliteDbConnection.dispose();
+    });
+
+    test('OK: Adminロールで、エンティティの削除ができた', async () => {
+      const result = await testClient?.mutate({
+        mutation: DELETE_USER,
+        variables: {
+          input: {
+            id: '3',
+          },
+        },
+      });
+
+      console.log('test', result);
+      const { user } = result?.data?.deleteUser ?? {};
+      expect(user?.email).toBe('anonymous@email.com');
+    });
+
+    test('NG: 存在しないIDを指定した', async () => {
+      const result = await testClient?.mutate({
+        mutation: DELETE_USER,
+        variables: {
+          input: {
+            id: '99999',
+          },
+        },
+      });
+
+      const { data, errors } = result ?? {};
+      expect(data?.deleteUser).toBeNull(); // dataはnullが返ってくる
+      expect(errors?.length).toBeGreaterThan(0); // errorsにエラー内容が含まれている
+    });
+
+    test('NG: 無効なパラメータを指定した', async () => {
+      const result = await testClient?.mutate({
+        mutation: DELETE_USER,
+        variables: {
+          input: {
+            hoge: 'fugafuga',
+          },
+        },
+      });
+
+      const { data, errors } = result ?? {};
+      expect(data).toBeUndefined(); // dataはundefinedが返ってくる（無効なリクエストのため）
+      expect(errors?.length).toBeGreaterThan(0); // errorsにエラー内容が含まれている
+    });
+  });
+
+  describe('Member', () => {
+    const actor = { id: '2', email: 'member@email.com', roles: [RoleTypes.Member] };
+    const sqliteDbConnection = new SqliteDbConnection();
+    let testClient: ApolloServerTestClient | undefined;
+
+    beforeAll(async () => {
+      const connection = await sqliteDbConnection.connect();
+      testClient = await createApolloServerForTesting(connection, actor);
+      await sqliteDbConnection.seedAll();
+    });
+
+    afterAll(async () => {
+      await sqliteDbConnection.dispose();
+    });
+
+    test('OK: 自身の削除ができた', async () => {
+      const result = await testClient?.mutate({
+        mutation: DELETE_USER,
+        variables: {
+          input: {
+            id: '2',
+          },
+        },
+      });
+
+      const { user } = result?.data?.deleteUser ?? {};
+      expect(user?.email).toBe('member@email.com');
+    });
+
+    test('NG: 失敗：Adminロールでないactorが操作した', async () => {
+      const result = await testClient?.mutate({
+        mutation: DELETE_USER,
+        variables: {
+          input: {
+            id: '1',
+          },
+        },
+      });
+
+      const { data, errors } = result ?? {};
+      expect(data?.deleteUser).toBeNull(); // dataはnullが返ってくる
+      expect(errors?.length).toBeGreaterThan(0); // errorsにエラー内容が含まれている
+    });
+  });
+});
