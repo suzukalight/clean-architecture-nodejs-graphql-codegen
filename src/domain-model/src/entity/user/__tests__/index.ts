a2a3a3cbb78c493695f90a637553d39a5e6fafe2
suzukalight <mail@mkubara.com>
2020-10-24 10:53:12 +0900

    Refactor userEntity tests

--- src/domain-model/src/entity/user/__tests__/index.ts
+++ src/domain-model/src/entity/user/__tests__/index.ts
@@ -16,8 +16,8 @@ describe('UserEntity', () => {
     test('OK: エンティティを生成できた', () => {
       const user = { id: '1', email: 'aaa@bbb.com', roles: [RoleTypes.Member] };
       const user1 = new UserEntity(user);
-      expect(user1.getId().toString()).toBe(user.id);
-      expect(user1.getEmail().toString()).toBe(user.email);
+      expect(user1.getId().isEqual(user.id)).toBeTruthy();
+      expect(user1.getEmail().isEqual(user.email)).toBeTruthy();
     });
 
     test('NG: idが不足しているため、失敗した', () => {
@@ -42,25 +42,29 @@ describe('UserEntity', () => {
   });
 
   describe('getter/setter', () => {
-    test('OK: Id', () => {
-      const user = new UserEntity({ id: '1', email: 'aaa@bbb.com', roles: [RoleTypes.Member] });
-      expect(user.getId().getId()).toBe('1');
+    const userDto = { id: '1', email: 'aaa@bbb.com', roles: [RoleTypes.Member] };
 
-      user.setId(new ID('999'));
-      expect(user.getId().getId()).toBe('999');
+    test('OK: Id', () => {
+      const user = new UserEntity(userDto);
+      expect(user.getId().isEqual(userDto.id)).toBeTruthy();
+
+      const newId = new ID('999');
+      user.setId(newId);
+      expect(user.getId().isEqual(newId)).toBeTruthy();
     });
 
     test('OK: Email', () => {
-      const user = new UserEntity({ id: '1', email: 'aaa@bbb.com', roles: [RoleTypes.Member] });
-      expect(user.getEmail().getEmail()).toBe('aaa@bbb.com');
+      const user = new UserEntity(userDto);
+      expect(user.getEmail().isEqual(userDto.email)).toBeTruthy();
 
-      user.setEmail(new Email('new_email@bbb.com'));
-      expect(user.getEmail().getEmail()).toBe('new_email@bbb.com');
+      const newEmail = new Email('new_email@bbb.com');
+      user.setEmail(newEmail);
+      expect(user.getEmail().isEqual(newEmail)).toBeTruthy();
     });
 
     test('OK: Roles', () => {
-      const user = new UserEntity({ id: '1', email: 'aaa@bbb.com', roles: [RoleTypes.Member] });
-      expect(user.getRoles()?.[0].toString()).toBe(RoleTypes.Member);
+      const user = new UserEntity(userDto);
+      expect(user.getRoles()?.[0].isEqual(RoleTypes.Member)).toBeTruthy();
 
       user.addRole(new Role(RoleTypes.Admin));
       expect(user.getRoles()?.length).toBe(2);
@@ -68,24 +72,23 @@ describe('UserEntity', () => {
       expect(user.getRoles()?.some((role) => role.isEqual(RoleTypes.Member))).toBeTruthy();
 
       user.removeRole(new Role(RoleTypes.Member));
-      console.log(user.getRoles());
       expect(user.getRoles()?.length).toBe(1);
       expect(user.getRoles()?.some((role) => role.isEqual(RoleTypes.Admin))).toBeTruthy();
       expect(user.getRoles()?.some((role) => role.isEqual(RoleTypes.Member))).toBeFalsy();
     });
 
     test('NG: すでに持っているロールを追加することはできない', () => {
-      const user = new UserEntity({ id: '1', email: 'aaa@bbb.com', roles: [RoleTypes.Member] });
+      const user = new UserEntity(userDto);
       expect(() => user.addRole(new Role(RoleTypes.Member))).toThrow(ConflictError);
     });
 
     test('NG: 持っていないロールを削除することはできない', () => {
-      const user = new UserEntity({ id: '1', email: 'aaa@bbb.com', roles: [RoleTypes.Member] });
+      const user = new UserEntity(userDto);
       expect(() => user.removeRole(new Role(RoleTypes.Admin))).toThrow(ConflictError);
     });
 
     test('NG: すべてのロールを削除することはできない', () => {
-      const user = new UserEntity({ id: '1', email: 'aaa@bbb.com', roles: [RoleTypes.Member] });
+      const user = new UserEntity(userDto);
       expect(() => user.removeRole(new Role(RoleTypes.Member))).toThrow(ValidationError);
     });
   });
