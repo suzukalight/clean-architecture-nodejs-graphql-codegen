1d84b59ff47512a746d65c0cf7545d15e58a7fba
suzukalight <mail@mkubara.com>
2020-10-27 18:34:23 +0900

    Use allTodosByOwnerId query service in resolver

--- src/backend/src/infrastructure/apollo-server/resolvers/entity/User.ts
+++ src/backend/src/infrastructure/apollo-server/resolvers/entity/User.ts
@@ -1,11 +1,11 @@
 import { UserResolvers } from 'schema';
 
-import { GqlTodoRepository } from '../../../../repository/typeorm/todo/repository/Todo';
+import { GqlTodoQueryService } from '../../../../repository/typeorm/todo/queryService/Todo';
 
 export const User: UserResolvers = {
   todos: async (parent, _args, ctx) => {
-    const repository = new GqlTodoRepository(ctx.dbConnection);
-    const todoEntities = await repository.allByOwnerId(parent.id); // FIXME: entity直接ではなく、usecaseを作ったほうが良い
-    return todoEntities ? todoEntities.map((todo) => todo.toDto()) : null;
+    const repository = new GqlTodoQueryService(ctx.dbConnection);
+    const result = await repository.allTodosByOwnerId({ ownerId: parent.id });
+    return result.todos;
   },
 };
