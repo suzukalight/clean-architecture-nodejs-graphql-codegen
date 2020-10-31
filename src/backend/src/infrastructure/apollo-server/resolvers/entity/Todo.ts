e9a3a99bcdc3aef6e7fcfcf4b38d022a5a7133ef
suzukalight <mail@mkubara.com>
2020-10-31 15:35:29 +0900

    Use GetUserById query service in todo resolver

--- src/backend/src/infrastructure/apollo-server/resolvers/entity/Todo.ts
+++ src/backend/src/infrastructure/apollo-server/resolvers/entity/Todo.ts
@@ -1,15 +1,22 @@
 import { TodoResolvers } from 'schema';
 
 import { ApolloServerContext } from '../../types';
-import { GqlUserRepository } from '../../../../repository/typeorm/user/repository/User';
-import { toGqlUser } from '../../../../presenter/utils/converter/user';
+import { GqlUserQueryService } from '../../../../repository/typeorm/user/queryService/User';
+import { allowOnlyWhenActorHasMemberRole, GetUserByIdInteractor } from 'domain-model';
+import { GqlGetUserByIdPresenter } from '../../../../presenter/user/GetUserById';
 
 export const Todo: TodoResolvers<ApolloServerContext> = {
-  owner: async (parent, _args, { dbConnection }) => {
+  owner: async (parent, _args, { actor, dbConnection }) => {
+    allowOnlyWhenActorHasMemberRole(actor);
+
     if (parent?.owner?.id) return parent.owner;
 
-    const repository = new GqlUserRepository(dbConnection);
-    const userEntity = await repository.getById(parent.ownerId); // FIXME: entity直接ではなく、usecaseを作ったほうが良い
-    return userEntity ? toGqlUser(userEntity.toDto()) : null;
+    const queryService = new GqlUserQueryService(dbConnection);
+    const presenter = new GqlGetUserByIdPresenter();
+    const usecase = new GetUserByIdInteractor(queryService, presenter);
+
+    await usecase.handle({ id: parent?.ownerId }, actor!);
+
+    return presenter.getResponse();
   },
 };
