f364bc32780264830e75338713d0d9cf66ea0ad4
suzukalight <mail@mkubara.com>
2020-10-31 15:35:54 +0900

    Use AllTodosByOwnerId usecase in user resolver

--- src/backend/src/infrastructure/apollo-server/resolvers/entity/User.ts
+++ src/backend/src/infrastructure/apollo-server/resolvers/entity/User.ts
@@ -1,11 +1,23 @@
 import { UserResolvers } from 'schema';
 
+import { ApolloServerContext } from '../../types';
 import { GqlTodoQueryService } from '../../../../repository/typeorm/todo/queryService/Todo';
+import { allowOnlyWhenActorHasMemberRole, AllTodosByOwnerIdInteractor } from 'domain-model';
+import { GqlAllTodosByOwnerIdPresenter } from '../../../../presenter/todo/AllTodosByOwnerId';
+import { toGqlTodo } from '../../../../presenter/utils/converter/todo';
 
-export const User: UserResolvers = {
-  todos: async (parent, _args, ctx) => {
-    const repository = new GqlTodoQueryService(ctx.dbConnection);
-    const result = await repository.allTodosByOwnerId({ ownerId: parent.id });
-    return result.todos;
+export const User: UserResolvers<ApolloServerContext> = {
+  todos: async (parent, _args, { actor, dbConnection }) => {
+    allowOnlyWhenActorHasMemberRole(actor);
+
+    if (parent?.todos?.length) return parent.todos;
+
+    const queryService = new GqlTodoQueryService(dbConnection);
+    const presenter = new GqlAllTodosByOwnerIdPresenter();
+    const usecase = new AllTodosByOwnerIdInteractor(queryService, presenter);
+
+    await usecase.handle({ ownerId: parent.id }, actor!);
+
+    return presenter.getResponse()?.edges?.map((edge) => toGqlTodo(edge.todo)) ?? [];
   },
 };
