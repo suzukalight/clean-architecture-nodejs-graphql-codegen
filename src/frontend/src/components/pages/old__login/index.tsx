a1f78259a8a13c952b8460b7e0ce1c663e9929d8
suzukalight <mail@mkubara.com>
2020-11-02 19:24:11 +0900

    Move old login component

--- src/frontend/src/components/pages/old__login/index.tsx
+++ src/frontend/src/components/pages/old__login/index.tsx
@@ -1,11 +1,14 @@
 import React, { useCallback, useContext } from 'react';
 import { Stack, PrimaryButton, Text, IStackTokens } from '@fluentui/react';
+import { Formik, Form, Field } from 'formik';
 import { useRouter } from 'next/router';
 
 import {
   useSignInEmailPasswordMutation,
   SignInEmailPasswordRequest,
 } from '../../../generated/graphql-client';
+import { loginValidationSchema } from './validation';
+import { FormikTextField } from '../../_formik/TextField';
 import { AuthContext } from '../../contexts/AuthContext';
 
 import styles from './index.module.scss';
@@ -17,15 +20,40 @@ const spacingToken: IStackTokens = {
 
 type SubmitFunction = (input: SignInEmailPasswordRequest) => void;
 type LoginFormProps = {
+  initialValues: SignInEmailPasswordRequest;
   onSubmit: SubmitFunction;
 };
 
-const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => (
-  <Stack>
-    <Stack tokens={spacingToken}>
-      <PrimaryButton type="submit">ログイン</PrimaryButton>
-    </Stack>
-  </Stack>
+const LoginForm: React.FC<LoginFormProps> = ({ initialValues, onSubmit }) => (
+  <Formik
+    onSubmit={onSubmit}
+    initialValues={initialValues}
+    validationSchema={loginValidationSchema}
+  >
+    {({ isSubmitting, isValid }) => (
+      <Form>
+        <Stack>
+          <Stack tokens={spacingToken}>
+            <Field component={FormikTextField} name="email" label="email" placeholder="email" />
+          </Stack>
+          <Stack tokens={spacingToken}>
+            <Field
+              component={FormikTextField}
+              name="password"
+              type="password"
+              label="password"
+              placeholder="password"
+            />
+          </Stack>
+          <Stack tokens={spacingToken}>
+            <PrimaryButton type="submit" disabled={isSubmitting || !isValid}>
+              ログイン
+            </PrimaryButton>
+          </Stack>
+        </Stack>
+      </Form>
+    )}
+  </Formik>
 );
 
 const Login = () => {
@@ -45,6 +73,11 @@ const Login = () => {
     [signInEmailPasswordMutation],
   );
 
+  const initialValues = {
+    email: '',
+    password: '',
+  };
+
   return (
     <Stack className={styles.wrap}>
       <Stack className={styles.main}>
@@ -52,7 +85,7 @@ const Login = () => {
           <Text variant="xLarge">TodoApp</Text>
         </Stack>
         <Stack>
-          <LoginForm onSubmit={handleSubmit} />
+          <LoginForm initialValues={initialValues} onSubmit={handleSubmit} />
         </Stack>
       </Stack>
     </Stack>
