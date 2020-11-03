eb129d4f9ba8aaa3f8ada81211a7668647fe343b
suzukalight <mail@mkubara.com>
2020-11-03 15:32:58 +0900

    Merge signin and signup mutation to signInOrSignUp

--- src/backend/src/infrastructure/apollo-server/resolvers/mutation/Auth/index.ts
+++ src/backend/src/infrastructure/apollo-server/resolvers/mutation/Auth/index.ts
@@ -3,12 +3,10 @@ import { MutationResolvers } from 'schema';
 import { ApolloServerContext } from '../../../types';
 import { signInEmailPassword } from './signInEmailPassword';
 import { signUpEmailPassword } from './signUpEmailPassword';
-import { signInAuth0 } from './signInAuth0';
-import { signUpAuth0 } from './signUpAuth0';
+import { signInOrSignUpAuth0 } from './signInOrSignUpAuth0';
 
 export const Auth: MutationResolvers<ApolloServerContext> = {
   ...signInEmailPassword,
   ...signUpEmailPassword,
-  ...signInAuth0,
-  ...signUpAuth0,
+  ...signInOrSignUpAuth0,
 };
--- src/backend/src/infrastructure/apollo-server/resolvers/mutation/Auth/signInOrSignUpAuth0.ts
+++ src/backend/src/infrastructure/apollo-server/resolvers/mutation/Auth/signInOrSignUpAuth0.ts
@@ -1,17 +1,17 @@
 import { MutationResolvers } from 'schema';
-import { SignUpAuth0Interactor } from 'domain-model';
+import { SignInOrSignUpAuth0Interactor } from 'domain-model';
 
 import { ApolloServerContext } from '../../../types';
 import { GqlUserRepository } from '../../../../../repository/typeorm/user/repository/User';
 import { GqlAuthAuth0Repository } from '../../../../../repository/typeorm/auth/repository/AuthAuth0';
-import { GqlSignUpAuth0Presenter } from '../../../../../presenter/auth/SignUpAuth0';
+import { GqlSignInOrSignUpAuth0Presenter } from '../../../../../presenter/auth/SignInOrSignUpAuth0';
 
-export const signUpAuth0: MutationResolvers<ApolloServerContext> = {
-  signUpAuth0: async (_parent, args, { dbConnection }) => {
+export const signInOrSignUpAuth0: MutationResolvers<ApolloServerContext> = {
+  signInOrSignUpAuth0: async (_parent, args, { dbConnection }) => {
     const userRepository = new GqlUserRepository(dbConnection);
     const authRepository = new GqlAuthAuth0Repository(dbConnection);
-    const presenter = new GqlSignUpAuth0Presenter();
-    const usecase = new SignUpAuth0Interactor(authRepository, userRepository, presenter);
+    const presenter = new GqlSignInOrSignUpAuth0Presenter();
+    const usecase = new SignInOrSignUpAuth0Interactor(authRepository, userRepository, presenter);
 
     await usecase.handle(args.input!);
 
